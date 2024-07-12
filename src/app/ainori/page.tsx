9932ad90847668f44504977ed693ef571eb50d70
koteyeah <k.kokuta2346@gmail.com>
2024-07-12 20:12:35 +0900

    相乗り取引の処理と状態遷移の実装

--- src/app/ainori/page.tsx
+++ src/app/ainori/page.tsx
@@ -1,78 +1,12 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { Button, Flex, Heading, VStack } from "../../common/design";
+import { Text, Button, Flex, Heading, VStack } from "../../common/design";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "@/src/lib/firebase/config";
 import { getDoc, doc, DocumentData } from "firebase/firestore";
-//状態によって表示する出力各種
-const LoadingComponent = () => (
-  <Flex align="center" justify="center" height="100vh" flexDirection="column">
-    <Heading>Loading...</Heading>
-  </Flex>
-);
-
-const AinoriComponent = ({
-  status,
-  userType,
-  otherUserData,
-  ainoriData,
-}: {
-  status: string;
-  userType: string;
-  otherUserData: DocumentData | null;
-  ainoriData: DocumentData | null;
-}) => {
-  return (
-    <Flex align="center" justify="center" height="100vh">
-      <VStack spacing={5}>
-        <Heading>ここにプログレスバー。現在の状態は{status}</Heading>
-
-        {otherUserData ? (
-          <Heading>ここに取引相手のプロフィールを表示</Heading>
-        ) : (
-          <Heading>相乗りを募集中です。</Heading>
-        )}
-        {ainoriData && <Heading>ここに相乗り情報を表示。</Heading>}
-
-        {status == "成立中" && userType == "ドライバー" && (
-          <>
-            <Heading>
-              相乗りが成立しています。乗客と合流し、乗車処理を行なってください。
-            </Heading>
-            <Heading>ここに更新ボタン</Heading>
-          </>
-        )}
-        {status == "成立中" && userType == "乗客" && (
-          <>
-            <Heading>
-              相乗りが成立しています。ドライバーと合流したら、乗車処理を行なってください。
-            </Heading>
-            <Heading>
-              乗車処理を行うボタンがここ。ここを押すと位置情報を取得してスタート地点の座標と合致していれば状態遷移
-            </Heading>
-          </>
-        )}
-        {status == "相乗り中" && userType == "ドライバー" && (
-          <>
-            <Heading>相乗り中です。安全に目的地まで相乗りを楽しんで！</Heading>
-            <Heading>ここに更新ボタン</Heading>
-          </>
-        )}
-        {status == "相乗り中" && userType == "乗客" && (
-          <>
-            <Heading>
-              相乗り中です。シートベルトをしっかりしてめ相乗りを楽しんで！目的地に着いたら必ず以下のボタンを押して相乗りを完了してください。
-            </Heading>
-            <Heading>
-              乗車完了を行うボタンがここ。ここを押すと位置情報を取得してゴール地点の座標と合致していれば状態遷移
-            </Heading>
-          </>
-        )}
-      </VStack>
-    </Flex>
-  );
-};
+import { getPositionDifference } from "./utils";
+import { Router } from "next/router";
 
 export default function Page() {
   const [userData, setUser] = useState<DocumentData | null>(null);
@@ -83,7 +17,62 @@ export default function Page() {
   >(null);
   const [ainoriData, setAinoriData] = useState<DocumentData | null>(null);
   const [userType, setUserType] = useState<"ドライバー" | "乗客" | null>(null);
+  const router = useRouter();
+  let isProcessing = false; // ボタン等の操作フラグを追加
 
+  const handleRideButton = async () => {
+    if (isProcessing) return; // 処理中であれば何もしない
+    isProcessing = true; // フラグを立てる
+    // NAISTの位置
+    const departureLat = 34.731922;
+    const departureLon = 135.73168;
+    try {
+      const isNear = await getPositionDifference(departureLat, departureLon);
+      if (isNear) {
+        console.log("出発地から500m以内です。相乗りを開始します。");
+        alert("出発地から500m以内です。相乗りを開始します。");
+        setStatus("相乗り中");
+        // データベースの処理を書く
+      } else {
+        console.log("出発地から離れています。");
+        alert("出発地から離れています。");
+        // 離れている場合の処理を追加
+      }
+    } catch (error) {
+      console.log("位置情報の取得に失敗しました。");
+      alert("位置情報の取得に失敗しました。");
+    } finally {
+      isProcessing = false; // 処理完了後にフラグを解除
+    }
+  };
+
+  const handleGetOffButton = async () => {
+    if (isProcessing) return; // 処理中であれば何もしない
+    isProcessing = true; // フラグを立てる
+    const destinationLat = 34.7249499;
+    const destinationLon = 135.7218884;
+    try {
+      const isNear = await getPositionDifference(
+        destinationLat,
+        destinationLon
+      );
+      if (isNear) {
+        console.log("目的地から500m以内です。相乗りを完了します。");
+        alert("目的地から500m以内です。相乗りを完了します。");
+        setStatus("相乗り終了");
+        // データベースの処理を記述
+      } else {
+        console.log("出発地から離れています。");
+        alert("出発地から離れています。");
+        // 離れている場合の処理を追加
+      }
+    } catch (error) {
+      console.error("位置情報の取得に失敗しました。");
+      alert("位置情報の取得に失敗しました。");
+    } finally {
+      isProcessing = false; // 処理完了後にフラグを解除
+    }
+  };
   useEffect(() => {
     // ユーザーのログイン状態を確認したのち、相乗り状況を取得し、格納
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
@@ -132,15 +121,100 @@ export default function Page() {
     });
     return () => unsubscribe();
   }, []);
-
+  const AinoriComponent = ({
+    status,
+    userType,
+    otherUserData,
+    ainoriData,
+  }: {
+    status: string | null;
+    userType: string | null;
+    otherUserData: DocumentData | null;
+    ainoriData: DocumentData | null;
+  }) => {
+    return (
+      <Flex align="center" justify="center" height="100vh">
+        <VStack spacing={5}>
+          {status && (
+            <Heading>ここにプログレスバー。現在の状態は{status}</Heading>
+          )}
+          {(status == "成立中" || status == "相乗り中") && (
+            <Text>ここに取引相手のプロフィールを表示</Text>
+          )}
+          {status == "募集中" && <Text>現在相乗りを募集しています。</Text>}
+          {ainoriData && <Text>ここに相乗り情報を表示。</Text>}
+          {status == "成立中" && (
+            <>
+              <Heading>
+                相乗りが成立しています。出発時刻に遅れないようにしましょう。
+              </Heading>
+              {userType === "ドライバー" ? (
+                <Button
+                  onClick={() => {
+                    router.refresh();
+                  }}
+                >
+                  更新
+                </Button>
+              ) : (
+                <>
+                  <Text>ドライバーと合流したら乗車するを押してください。</Text>
+                  <Button onClick={handleRideButton}>乗車する</Button>
+                </>
+              )}
+            </>
+          )}
+          {status == "相乗り中" && (
+            <>
+              <Heading>
+                相乗り中です。目的地まで安全に相乗りを楽しんで！
+              </Heading>
+              {userType === "ドライバー" ? (
+                <Button
+                  onClick={() => {
+                    console.log("aaa");
+                    router.refresh();
+                  }}
+                >
+                  更新
+                </Button>
+              ) : (
+                <>
+                  <Text>目的地に到着したら降車するを押してください</Text>
+                  <Button onClick={handleGetOffButton}>降車する</Button>
+                </>
+              )}
+            </>
+          )}
+          {status == "相乗り終了" && userType == "乗客" && (
+            <>
+              <Heading>
+                相乗りは完了しました！またのご利用お待ちしています！
+              </Heading>
+              <Text fontSize="3xl">別のページに移動してください</Text>
+            </>
+          )}
+        </VStack>
+      </Flex>
+    );
+  };
   if (loading) {
-    return <LoadingComponent />;
+    return (
+      <Flex
+        align="center"
+        justify="center"
+        height="100vh"
+        flexDirection="column"
+      >
+        <Heading>Loading...</Heading>
+      </Flex>
+    );
   }
 
   return (
     <AinoriComponent
-      status={"成立中"}
-      userType={"ドライバー"}
+      status={status}
+      userType={userType}
       otherUserData={null}
       ainoriData={null}
     />
--- src/app/ainori/utils.ts
+++ src/app/ainori/utils.ts
@@ -1,44 +1,49 @@
-export function getPositionDifference(lat: number, lon: number) {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+export async function getPositionDifference(
+  lat: number,
+  lon: number
+): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
 
-        // 現在地と目的地の距離を計算する関数
-        const calculateDistance = (
-          lat1: number,
-          lon1: number,
-          lat2: number,
-          lon2: number
-        ) => {
-          const R = 6371e3; // Radius of the Earth in meters
-          const φ1 = lat1 * (Math.PI / 180); // φ, λ in radians
-          const φ2 = lat2 * (Math.PI / 180);
-          const Δφ = (lat2 - lat1) * (Math.PI / 180);
-          const Δλ = (lon2 - lon1) * (Math.PI / 180);
+          // 現在地と目的地の距離を計算する関数
+          const calculateDistance = (
+            lat1: number,
+            lon1: number,
+            lat2: number,
+            lon2: number
+          ) => {
+            const R = 6371e3; // Radius of the Earth in meters
+            const φ1 = lat1 * (Math.PI / 180); // φ, λ in radians
+            const φ2 = lat2 * (Math.PI / 180);
+            const Δφ = (lat2 - lat1) * (Math.PI / 180);
+            const Δλ = (lon2 - lon1) * (Math.PI / 180);
 
-          const a =
-            Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-            Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-          const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            const a =
+              Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+              Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-          const distance = R * c; // Distance in meters
-          return distance;
-        };
+            const distance = R * c; // Distance in meters
+            return distance;
+          };
 
-        // 目的地と現在地の距離を計算
-        const distance = calculateDistance(lat, lon, latitude, longitude);
-        console.log("目的地と現在地の距離:", distance, "m");
-        const isOK = distance <= 500;
-        return isOK;
-      },
-      (error) => {
-        console.error("位置情報の取得に失敗しました:", error);
-        return false;
-      }
-    );
-  } else {
-    console.error("位置情報の取得がサポートされていません。");
-    return false;
-  }
+          // 目的地と現在地の距離を計算
+          const distance = calculateDistance(lat, lon, latitude, longitude);
+          console.log("目的地と現在地の距離:", distance, "m");
+          const isOK = distance <= 500;
+          resolve(isOK);
+        },
+        (error) => {
+          console.error("位置情報の取得に失敗しました:", error);
+          reject(error);
+        }
+      );
+    } else {
+      console.error("位置情報の取得がサポートされていません。");
+      reject(new Error("位置情報の取得がサポートされていません。"));
+    }
+  });
 }
