b3d812cccf10fcdf40fee7f58e830cdb33ae2856
koteyeah <k.kokuta2346@gmail.com>
2024-07-13 23:20:45 +0900

    キャンセル処理でページ遷移するように

--- src/app/ainori/page.tsx
+++ src/app/ainori/page.tsx
@@ -285,6 +285,7 @@ export default function Page() {
                     });
                   }
                   alert("取引をキャンセルしました。");
+                  router.push("/postList");
                 }}
               >
                 取引をキャンセル
