b84bdf7e0a4d410f8d8e2d8e0c634c25c6215a93
Momoka <hirose.momoka.hm4@naist.ac.jp>
2024-08-13 17:05:23 +0900

    [add]評価ボタン設置，データ消去必要

--- src/app/evaluation/page.tsx
+++ src/app/evaluation/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { Button } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+
 export default function Page() {
-  return <>ここに評価ページが来るよ</>;
+  const router = useRouter();
+
+  const handleComplete = () => {
+    router.push('/add');
+  };
+
+  return (
+    <>
+      <p>ここに評価ページが来るよ</p>
+      <Button colorScheme="teal" mt={4} onClick={handleComplete}>
+        評価完了
+      </Button>
+    </>
+  );
 }
